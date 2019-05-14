59529c3b063b78caca44deb490b3424190b9bd66
Nguyễn Hồng Quyên <hong.quyen.ptit@gmail.com>
2019-05-14 15:18:00 +0700

    add chart?

--- app/src/main/java/com/example/qlct/MainActivity.java
+++ app/src/main/java/com/example/qlct/MainActivity.java
@@ -12,6 +12,18 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.LargeValueFormatter;
+
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -40,8 +52,14 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rbMonth;
     @BindView(R.id.rb_year)
     RadioButton rbYear;
+    @BindView(R.id.chart)
+    BarChart chart;
 
     private boolean isText = true;
+
+    private float barWidth = 0.3f;
+    private float barSpace = 0f;
+    private float groupSpace = 0.4f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +67,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_overview);
         ButterKnife.bind(this);
         fillData();
-        init();
+        initView();
+        initChart();
     }
 
     private void fillData() {
@@ -57,7 +76,84 @@ public class MainActivity extends AppCompatActivity {
         loChart.setVisibility(View.GONE);
     }
 
-    private void init() {
+    private void initChart() {
+        chart.setDescription(null);
+        chart.setPinchZoom(false);
+        chart.setScaleEnabled(false);
+        chart.setDrawBarShadow(false);
+        chart.setDrawGridBackground(false);
+
+        int groupCount = 6;
+
+        ArrayList xVals = new ArrayList();
+
+        xVals.add("Jan");
+        xVals.add("Feb");
+        xVals.add("Mar");
+        xVals.add("Apr");
+        xVals.add("May");
+        xVals.add("Jun");
+
+        ArrayList yVals1 = new ArrayList();
+        ArrayList yVals2 = new ArrayList();
+
+        yVals1.add(new BarEntry(1, (float) 1));
+        yVals2.add(new BarEntry(1, (float) 2));
+        yVals1.add(new BarEntry(2, (float) 3));
+        yVals2.add(new BarEntry(2, (float) 4));
+        yVals1.add(new BarEntry(3, (float) 5));
+        yVals2.add(new BarEntry(3, (float) 6));
+        yVals1.add(new BarEntry(4, (float) 7));
+        yVals2.add(new BarEntry(4, (float) 8));
+        yVals1.add(new BarEntry(5, (float) 9));
+        yVals2.add(new BarEntry(5, (float) 10));
+        yVals1.add(new BarEntry(6, (float) 11));
+        yVals2.add(new BarEntry(6, (float) 12));
+
+        BarDataSet set1, set2;
+        set1 = new BarDataSet(yVals1, "A");
+        set1.setColor(Color.RED);
+        set2 = new BarDataSet(yVals2, "B");
+        set2.setColor(Color.BLUE);
+        BarData data = new BarData(set1, set2);
+        data.setValueFormatter(new LargeValueFormatter());
+        chart.setData(data);
+        chart.getBarData().setBarWidth(barWidth);
+        chart.getXAxis().setAxisMinimum(0);
+        chart.getXAxis().setAxisMaximum(0 + chart.getBarData().getGroupWidth(groupSpace, barSpace) * groupCount);
+        chart.groupBars(0, groupSpace, barSpace);
+        chart.getData().setHighlightEnabled(false);
+        chart.invalidate();
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(true);
+        l.setYOffset(20f);
+        l.setXOffset(0f);
+        l.setYEntrySpace(0f);
+        l.setTextSize(8f);
+
+        //X-axis
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisMaximum(6);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xVals));
+//Y-axis
+        chart.getAxisRight().setEnabled(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setValueFormatter(new LargeValueFormatter());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setSpaceTop(35f);
+        leftAxis.setAxisMinimum(0f);
+    }
+
+    private void initView() {
         rdgTime.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @SuppressLint("ResourceAsColor")
             @Override
